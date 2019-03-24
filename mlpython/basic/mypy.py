059b5aff2ada8d41cec525163949867863c07dd1
jianlianhe <weberw443@gmail.com>
2019-03-24 09:54:40 +0800

    add note

--- mlpython/basic/mypy.py
+++ mlpython/basic/mypy.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from string import Template 
+
 print(10//6)
 print(10/6)
 
@@ -10,3 +12,11 @@ for elem in seq:
 
 for i, elem in enumerate(seq):
     print(i, elem)
+
+print('a' in 'abc')
+print('e' in 'abc')
+
+print('*'*50)
+
+s = Template('there are ${num} dogs')
+print (s.substitute(num=3))
